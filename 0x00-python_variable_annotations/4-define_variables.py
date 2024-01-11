0a25397bbf396c937e7381b6e53f8338da34f4f3
kumdan job <redeks123456@gmail.com>
2024-01-11 22:13:04 +0100

    python annotations

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -5,12 +5,3 @@ a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
 school: str = "Holberton"
-
-print("a is a {} with a value of {}".format(type(a), a))
-print("pi is a {} with a value of {}".format(type(pi), pi))
-print(
-    "i_understand_annotations is a {} with a value of {}".format(
-        type(i_understand_annotations), i_understand_annotations
-    )
-)
-print("school is a {} with a value of {}".format(type(school), school))
