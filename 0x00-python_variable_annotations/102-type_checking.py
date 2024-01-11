cb53385403c1fc5106781b50272ce2fec85cdb03
kumdan job <redeks123456@gmail.com>
2024-01-11 22:59:48 +0100

    python annotations

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,14 +1,14 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 """0. Basic annotations - add"""
 # filename: 102-type_checking.py
 
-from typing import Tuple
+from typing import Tuple, List
 
 
-def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """0. Basic annotations - add"""
     zoomed_in: Tuple = tuple([item for item in lst for i in range(factor)])
-    return zoomed_in
+    return list(zoomed_in)
 
 
 array = tuple([12, 72, 91])
@@ -16,3 +16,6 @@ array = tuple([12, 72, 91])
 zoom_2x = zoom_array(array)
 
 zoom_3x = zoom_array(array, 3)
+
+
+print(zoom_array.__annotations__)
