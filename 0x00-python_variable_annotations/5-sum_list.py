23318ae84c444678f09e6a7df4972e5c8676bb6d
kumdan job <redeks123456@gmail.com>
2024-01-11 22:25:14 +0100

    python annotations

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """0. Basic annotations - add"""
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """0. Basic annotations - add"""
     return sum(input_list)
