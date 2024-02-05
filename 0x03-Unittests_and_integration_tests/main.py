3abf10737a2b5e5f5b7383a3840ac807799f01b4
kumdan job <redeks123456@gmail.com>
2024-02-05 23:26:51 +0100

    unittest

--- 0x03-Unittests_and_integration_tests/main.py
+++ 0x03-Unittests_and_integration_tests/main.py
@@ -1,7 +1,6 @@
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 print(access_nested_map({"a": {"b": {"c": 1}}}, ["a", "b", "c"]))
 print(access_nested_map(nested_map={"a": 1}, path=("a",)))
 print(access_nested_map(nested_map={"a": {"b": 2}}, path=("a",)))
 print(access_nested_map(nested_map={"a": {"b": 2}}, path=("a", "b")))
-#
